91a82a78d6ac8ede7c23e0d9b8533c995fc48542
Yu, Kenneth <Kenneth.Yu@priceline.com>
2020-07-23 00:12:47 -0400

    fixes label rendering logic

--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -3,15 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import ResultComponent from '../components/ResultComponent'
 
 class SearchResults extends Component {
-
     render(){
         let parsedResults = this.props.searchResults.map((resultDetails, index) => <ResultComponent key={index} resultDetails={resultDetails} saveHandler={this.props.saveHandler}/>)
         let parsedSavedGems = this.props.savedGems.map((savedGems, index) => <ResultComponent key={index} savedDetails={savedGems} saveHandler={this.props.saveHandler}/>)
+
         return(
             <div className="SearchResults">
-                {parsedResults === [] ? <h1>Search Results</h1> : null}
+                {parsedResults.length > 0 ? <h1>Search Results</h1> : null}
                 {parsedResults}
-                <h1>Saved Gems</h1>
+                {parsedSavedGems.length > 0 ? <h1>Saved Gems</h1> : null}
                 {parsedSavedGems}
 
             </div>
