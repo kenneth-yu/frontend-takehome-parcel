3248fd26e443d09d66190b67e18aef227825dfee
Yu, Kenneth <Kenneth.Yu@priceline.com>
2020-07-27 00:25:47 -0400

    adds ability to search using enter

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,12 +4,14 @@ import { Button, InputGroup, Form, Row} from 'react-bootstrap';
 class SearchBox extends Component {
     render(){
         return(
+        <Form onSubmit={this.props.searchHandler}>
             <InputGroup className="mb-5 mt-5">
-                <Form.Control  id="searchTextBox" placeholder="Search Ruby Gems..." value={this.props.searchText} onChange={this.props.changeHandler} size="lg" type="text" />
-                <InputGroup.Append>
-                    <Button className="pl-4 pr-4" type="submit" variant="outline-primary" id="searchButton" onClick={this.props.searchHandler}>Search</Button>
-                </InputGroup.Append>
+                    <Form.Control id="searchTextBox" placeholder="Search Ruby Gems..." value={this.props.searchText} onChange={this.props.changeHandler} size="lg" type="text" />
+                    <InputGroup.Append>
+                        <Button className="pl-4 pr-4" type="submit" variant="outline-primary" id="searchButton" >Search</Button>
+                    </InputGroup.Append>
             </InputGroup>
+        </Form>
         )
     }
 }
